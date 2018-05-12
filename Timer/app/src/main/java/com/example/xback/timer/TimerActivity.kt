f9f41034d2a4b96a1730062282b1bde44af25db9
mhendylazuardi <mochammad.lazuardi@students.amikom.ac.id>
2018-05-12 12:24:21 +0700

    second

--- Timer/app/src/main/java/com/example/xback/timer/TimerActivity.kt
+++ Timer/app/src/main/java/com/example/xback/timer/TimerActivity.kt
@@ -1,6 +1,7 @@
 package com.example.xback.timer
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -10,21 +11,61 @@ import kotlinx.android.synthetic.main.activity_timer.*
 
 class TimerActivity : AppCompatActivity() {
 
+    enum class TimerState(
+            Stopped, Pause, Running
+    )
+
+    private lateinit var timer : CountDownTimer
+    private timerLengthSeconds = 0L
+    private timerState =TimerState.Stopped
+
+    private var secondRemaining = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
         setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "       Timer"
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab_start.setOnClickListener{v ->
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
         }
+        fab_pause.setOnClickListener { v ->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+        fab_stop.setOnClickListener { v ->
+            timer.cancel()
+            onTimerFinished()
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        initTimer()
+
+//        remove background timer, hide notification
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_timer, menu)
         return true
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if(timerState == TimerState.Running) {
+            timer.cancel()
+//            start background timer and show notificatin
+        }else if(timerState == TimerState.Paused)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
